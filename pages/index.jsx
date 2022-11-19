5d80059e11c39261f3b30219e23648c2e9953e16
Madhuran Thiagarajah <madhuran.v@gmail.com>
2022-11-19 14:23:45 -0500

    add collection filtering

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,34 +2,36 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react';
 import { Network, Alchemy } from "alchemy-sdk";
-require('dotenv').config()
-
-
+//require('dotenv').config();
 
 const Home = () => {
   const [wallet, setWalletAddress] = useState("");
   const [collection, setCollectionAddress] = useState("");
-  const [NFTs, setNFTs] =useState([]);
+  const [NFTs, setNFTs] = useState([]);
 
   const fetchNFTs = async() => {
     let nfts;
     console.log("fetching nfts");
+
+    const settings = {
+      apiKey: process.env.ALCHEMY_APIKEY, 
+      network: process.env.NETWORKURL 
+    };
+    
+    const alchemy = await new Alchemy(settings);
+
     if(!collection.length) {
-
-      const settings = {
-        apiKey: process.env.ALCHEMY_APIKEY, // Replace with your Alchemy API Key.
-        network: process.env.NETWORKURL // Replace with your network.
-      };
-      
-      const alchemy = await new Alchemy(settings);
-
-  
       nfts = await alchemy.nft.getNftsForOwner(wallet);
     } else {
-
+      console.log("fetching nfts for collection owned by address")
+      nfts = await alchemy.nft.getNftsForOwner(wallet, {
+        contractAddresses: [collection],
+      });
     }
+
     if (nfts) {
       console.log(nfts)
+      setNFTs(nfts)
     }
   }
 
@@ -37,7 +39,7 @@ const Home = () => {
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
        <div>
         <input onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
-        <input onChange={(e)=>{setCollectionAddress(e)}} type={"text"} placeholder="Add the collection address"></input>
+        <input onChange={(e)=>{setCollectionAddress(e.target.value)}} type={"text"} placeholder="Add the collection address"></input>
         <label><input type={"checkbox"}></input></label>
         <button onClick={
           () => {
