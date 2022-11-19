847dbe27fd52172581574444ba291c976955f7bf
Madhuran Thiagarajah <madhuran.v@gmail.com>
2022-11-19 15:31:14 -0500

    Add fetch for collections

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,7 @@ const Home = () => {
   const [wallet, setWalletAddress] = useState("");
   const [collection, setCollectionAddress] = useState("");
   const [NFTs, setNFTs] = useState([]);
+  const [fetchForCollection, setFetchForCollection] = useState(false);
 
   const fetchNFTs = async() => {
     let nfts;
@@ -25,7 +26,7 @@ const Home = () => {
     } else {
       console.log("fetching nfts for collection owned by address")
       nfts = await alchemy.nft.getNftsForOwner(wallet, {
-        contractAddresses: [collection],
+        contractAddresses: [collection]
       });
     }
 
@@ -33,17 +34,34 @@ const Home = () => {
       console.log(nfts)
       setNFTs(nfts)
     }
-  }
+  };
+
+  const fetchNFTsForCollection = async () => {
+    const settings = {
+      apiKey: process.env.ALCHEMY_APIKEY, 
+      network: process.env.NETWORKURL,
+      withMetadata: true 
+    };
+    if (collection.length) {
+      const alchemy = await new Alchemy(settings);
+      const nfts = await alchemy.nft.getNftsForContract(collection).then(console.log);
+      };
+      
+    }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
        <div>
         <input onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
         <input onChange={(e)=>{setCollectionAddress(e.target.value)}} type={"text"} placeholder="Add the collection address"></input>
-        <label><input type={"checkbox"}></input></label>
+        <label><input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"}></input>Fetch for collection</label>
         <button onClick={
           () => {
-             fetchNFTs()
+            if(fetchForCollection) {
+              fetchNFTsForCollection();
+            } else {
+              fetchNFTs();
+            }
           }
         }>Let's go! </button>
       </div>
